2d322864ec0d68fc8e2705e91ae23c825f99c43d
Laurent Montel <montel@kde.org>
2009-12-09 08:29:54 +0000

    SVN_SILENT: minor
    svn path=/trunk/KDE/kdegames/granatier/; revision=1060499

--- src/Messages.sh
+++ src/Messages.sh
@@ -8,4 +8,6 @@ $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cc, .cpp, and .h, just add them in the find call.
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/granatier.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/granatier.pot
+
+rm -f rc.cpp
