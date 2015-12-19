5fe704f0688af691233ba1ed167f196051135d36
Frederik Schwarzer <schwarzer@kde.org>
2015-12-19 13:11:36 +0100

    Remove duplicate include.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -31,7 +31,6 @@
 #include <QTimer>
 #include <KActionCollection>
 #include <KStandardGameAction>
-#include <KToggleAction>
 #include <KMessageBox>
 #include <KConfigDialog>
 #include <KLocalizedString>
