f7d821fac0289c15a8ab60808a4c257409e1871f
Friedrich W. H. Kossebau <kossebau@kde.org>
2022-09-19 20:31:12 +0200

    Update homepage to apps.kde.org
    GIT_SILENT

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,10 @@ int main(int argc, char** argv)
     KAboutData about(QStringLiteral("granatier"),  i18n("Granatier"),
         QStringLiteral(GRANATIER_VERSION_STRING),
         i18n("Granatier is a Bomberman Clone!"),
-        KAboutLicense::GPL, i18n("Copyright (c) 2009-2015 The Granatier Team!"));
+        KAboutLicense::GPL, i18n("Copyright (c) 2009-2015 The Granatier Team!"),
+        QString(),
+        QStringLiteral("https://apps.kde.org//granatier")
+    );
     about.addAuthor(i18n("Mathias Kraus"), i18n("Maintainer"), QStringLiteral("k.hias@gmx.de"));
     about.addCredit(i18n("Arturo Silva"), i18n("Most of the beautiful granatier graphics"));
     about.addCredit(i18n("Thomas Gallinari"), i18n("Developer of Kapman, which was the base of Granatier"), QStringLiteral("tg8187@yahoo.fr"));
@@ -85,7 +88,6 @@ int main(int argc, char** argv)
     about.addCredit(i18n("Romain Castan"), i18n("Developer of Kapman, which was the base of Granatier"), QStringLiteral("romaincastan@gmail.com"));
     about.addCredit(i18n("Alexandre Galinier"), i18n("Developer of Kapman, which was the base of Granatier"), QStringLiteral("alex.galinier@gmail.com"));
     about.addCredit(i18n("Nathalie Liesse"), i18n("Developer of Kapman, which was the base of Granatier"), QStringLiteral("nathalie.liesse@gmail.com"));
-    about.setHomepage(QStringLiteral("https://www.kde.org/applications/games/granatier/"));
     // Command line arguments
     QCommandLineParser parser;
     KAboutData::setApplicationData(about);
